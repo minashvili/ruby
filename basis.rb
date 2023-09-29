00a1f6dd7a528a0591898c0d0e0fcece4fef734b
minashvili <minashvili>
2023-09-29 20:22:28 +0300

    Решаю простые задачи №3

--- basis.rb
+++ basis.rb
@@ -1,67 +1,17 @@
-# Непростая монетка
-# Модифицируйте программу из предыдущей задачи так, чтобы иногда выпадало еще и "ребро"
-# («Монета встала на ребро»). Причем ребро должно выпадать намного реже, чем орел или решка
-# (вероятность 1/10 или меньше).
+# Среднее арифметическое трех чисел
+# Напишите программу, которая находит среднее арифметическое трех, введенных пользователем целых чисел.
 
-def randomo_bot()
-  value_list = [0, 1, 2]
-  return value_list.sample
+
+def summator_number()
+  hash_word = []
+  (1..3).each { |x|
+    puts "Введите число"
+    hash_word << gets.to_i
+  }
+  puts "Среднее Арефметическое", hash_word.sum / hash_word.length
 end
 
-def random_coun(randomo_bot_value, number_of_attempts = 2)
-  if randomo_bot_value == 2 && number_of_attempts != 0
-    number_of_attempts = number_of_attempts - 1
-    random_coun(randomo_bot(), number_of_attempts)
-  else
-    case randomo_bot_value
-    when 0
-      puts "Выпал Орел"
-    when 1
-      puts "Выпала Решка"
-    when 2
-      puts "Супер ситуация выпало Ребро"
-    end
-  end
-end
-
-
-random_coun(randomo_bot)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def random_coun()
-#   last_value = ''
-#   value_list = [0, 1, 2]
-#   god_of_random = value_list.sample
-#   case god_of_random
-#   when 0
-#     last_value = 'Орел'
-#   when 1
-#     last_value = 'Решка'
-#   when 2
-#     last_value = 'Ребро'
-#
-#   end
-#   puts "Выпало #{last_value}"
-# end
-#
-# random_coun()
-
-
+summator_number()
 
 
 
