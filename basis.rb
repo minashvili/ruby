2da678f44c048552215d8d4aee4966c3f0e978a5
minashvili <minashvili>
2023-09-29 17:41:23 +0300

    Решаю простые задачи

--- basis.rb
+++ basis.rb
@@ -1,7 +1,23 @@
+#Напишите программу, которая подсказывает, выходной сегодня или нет
+# (для простоты мы не учитываем государственных праздников, итак много дней для безделья):
 
-def count_plays(year)
-  s = get_shakey
-  s["William Shakespeare"].select { |k, v| v["finished"] == year }.each { |key, val| puts val["title"] }.count
+require 'date'
+
+def functio_check
+
+  now_date = Date.today
+  if now_date.wday > 5
+    puts 'Сегодня выходной', now_date.to_s
+  else
+    puts 'Сегодня еще не выходной', now_date.to_s
+  end
+
 end
+
+functio_check()
+
+
+
+
 
 
