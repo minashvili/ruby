ebfd7c38d5d7f6e910dcc4ec617969d20bee6154
minashvili <minashvili>
2023-10-01 00:06:31 +0300

    Разбираю класс (офсайт)

--- basis.rb
+++ basis.rb
@@ -1,4 +1,3 @@
-
 class Blurb
   attr_accessor :content, :time, :mood
 
@@ -7,12 +6,39 @@ class Blurb
     @content = content[0..39]
     @mood    = mood
   end
+
+  def moodify
+    if    @mood == :sad
+      return ":-("
+    elsif @mood == :happy
+      return ":-)"
+      # Добавь другой смайлик сюда
+    end
+
+    # Смайлик по умолчанию
+    ":-|"
+  end
 end
 
-my_blurb = Blurb.new(:sick).time
+class Blurbalizer
+  def initialize(title)
+    @title  = title
+    @blurbs = []
+  end
 
-puts my_blurb
+  def add_a_blurb(mood, content)
+    @blurbs << Blurb.new(mood, content)
+  end
 
+  def show_timeline
+    puts "Blurbalizer: #{@title} содержит #{@blurbs.count} Blurbs"
 
+    @blurbs.sort_by { |t|
+      t.time
+    }.reverse.each { |t|
+      puts "#{t.content.ljust(40)} #{t.time}"
+    }
+  end
+end
 
-
+myapp.show_timeline
